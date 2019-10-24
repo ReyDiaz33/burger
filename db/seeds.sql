d3f91c8897177b0c8453db2688153929a64b2d8b
Rey Diaz <reybdiazjr@gmail.com>
2019-10-24 11:51:03 -0400

    added handle bars main and index htmls and set up connection and orm files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
 -- In this file, write insert queries to populate the burgers 
 -- table with at least three entries.
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("hamburger", false), ("cheeseburger", false), ("veggieburger", false);
